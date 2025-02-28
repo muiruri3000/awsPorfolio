73fffada179a9f7b2c2f4141a04220094cce0a08
muiruri3000 <josee.muiruri95@gmail.com>
2025-02-28 08:00:49 +0300

    conditionals if elif

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+bananas = int(input("Enter the bananas you have: "))
+
+if bananas >= 5:
+    print("I have a bunch of bananas")
+    
+elif bananas >= 1 and bananas < 5:
+    print("i have a small bunch of bananas")
+else:
+    print("I have no bananas")
